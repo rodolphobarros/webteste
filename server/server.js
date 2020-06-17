7a7bd3e7bc2d55fe3eb5c7e63ea6c9c7e8699538
rodolpho <rodolphobarros@gmail.com>
2020-06-17 16:33:22 +0100

    teste

--- server/server.js
+++ server/server.js
@@ -1,23 +1,18 @@
-// Copyright IBM Corp. 2016,2019. All Rights Reserved.
-// Node module: loopback-workspace
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
 'use strict';
 
-const loopback = require('loopback');
-const boot = require('loopback-boot');
+var loopback = require('loopback');
+var boot = require('loopback-boot');
 
-const app = module.exports = loopback();
+var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    const baseUrl = app.get('url').replace(/\/$/, '');
+    var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
-      const explorerPath = app.get('loopback-component-explorer').mountPath;
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
